11569c314f185ef2162c319508ec473194fcf6dc
dimkk <freeaces@icloud.com>
2021-07-17 07:22:42 +0300

    - remove vue

--- packages/@headlessui-vue/src/utils/dom.ts
+++
@@ -1,7 +0,0 @@
-import { Ref } from 'vue'
-
-export function dom<T extends HTMLElement>(ref?: Ref<T | null>): T | null {
-  if (ref == null) return null
-  if (ref.value == null) return null
-  return ((ref as Ref<T & { $el: unknown }>).value.$el ?? ref.value) as T | null
-}
